a3311fa7c9d6d8c31902630f31c0000bdf833b57
Nenshu <nenshu.dev@gmail.com>
2025-11-05 08:07:07 -0600

    LeafNode and Unit Tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
 import unittest
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_multiple(self):
@@ -20,6 +21,28 @@ class TestHTMLNode(unittest.TestCase):
     def test_to_html_raises(self):
         with self.assertRaises(NotImplementedError):
             HTMLNode(tag="p", value="x").to_html()
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a_with_href(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_leaf_no_tag_returns_raw_text(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
+
+    def test_leaf_raises_without_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 if __name__ == "__main__":
     unittest.main()
